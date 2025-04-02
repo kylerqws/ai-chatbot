cc3b76174591d7bcaa1a79173ae6b04dcdaef170
Rodion Kachkin <kyleRQWS@gmail.com>
2025-04-02 21:39:23 +0400

    Create file service contract in openai package

---
+++ pkg/openai/contract/service/file.go
@@ -0,0 +1,36 @@
+package service
+
+import "context"
+
+type File struct {
+	ID        string `json:"id"`
+	Object    string `json:"object"`
+	Bytes     int    `json:"bytes"`
+	CreatedAt int64  `json:"created_at"`
+	Filename  string `json:"filename"`
+	Purpose   string `json:"purpose"`
+}
+
+type UploadFileRequest struct {
+	FilePath string
+	Purpose  string
+}
+
+type GetFileInfoRequest struct {
+	FileID string
+}
+
+type DeleteFileRequest struct {
+	FileID string
+}
+
+type ListFilesRequest struct {
+	// filters may be added in the future
+}
+
+type FileService interface {
+	UploadFile(ctx context.Context, req *UploadFileRequest) (string, error)
+	GetFileInfo(ctx context.Context, req *GetFileInfoRequest) (*File, error)
+	ListFiles(ctx context.Context, req *ListFilesRequest) ([]*File, error)
+	DeleteFile(ctx context.Context, req *DeleteFileRequest) error
+}
