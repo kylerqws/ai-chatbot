a19e34d302565e6fd18ec8ea637a9107aa4e914d
Rodion Kachkin <kyleRQWS@gmail.com>
2025-04-05 23:20:00 +0400

    Create internal logger package

---
+++ internal/logger/logger.go
@@ -0,0 +1,12 @@
+package logger
+
+import (
+	"context"
+
+	"github.com/kylerqws/chatbot/pkg/logger"
+	ctr "github.com/kylerqws/chatbot/pkg/logger/contract"
+)
+
+func New(ctx context.Context) (ctr.Logger, error) {
+	return logger.NewLogger(ctx)
+}
