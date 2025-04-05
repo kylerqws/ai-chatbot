b4713f270af1b00350d40b01d89942a3257f9181
Rodion Kachkin <kyleRQWS@gmail.com>
2025-04-05 19:39:04 +0400

    Add error message to openai main file

--- pkg/openai/openai.go
+++ pkg/openai/openai.go
@@ -2,32 +2,34 @@ package openai
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kylerqws/chatbot/pkg/openai/infrastructure/client"
 	"github.com/kylerqws/chatbot/pkg/openai/infrastructure/config"
 	"github.com/kylerqws/chatbot/pkg/openai/usecase/service"
 
+	ctr "github.com/kylerqws/chatbot/pkg/openai/contract"
 	ctrsrv "github.com/kylerqws/chatbot/pkg/openai/contract/service"
 )
 
-type API struct {
+type api struct {
 	fileService ctrsrv.FileService
 }
 
-func New(ctx context.Context) (*API, error) {
+func New(ctx context.Context) (ctr.OpenAI, error) {
 	cfg, err := config.New(ctx)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create OpenAI config: %w", err)
 	}
 
 	cl := client.New(cfg)
 	sf := service.NewFactory(cl, cfg)
 
-	return &API{
+	return &api{
 		fileService: sf.FileService(),
 	}, nil
 }
 
-func (api *API) FileService() ctrsrv.FileService {
+func (api *api) FileService() ctrsrv.FileService {
 	return api.fileService
 }
