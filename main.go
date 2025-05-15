fc338813c1897427df7411aa9cff5d2fe20f5aa8
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-15 19:40:21 +0400

    Commit mew main.go

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/kylerqws/chatgpt-bot/cmd"
+
+	intapp "github.com/kylerqws/chatbot/internal/app"
+	intres "github.com/kylerqws/chatbot/internal/app/resolver"
+	intcli "github.com/kylerqws/chatbot/internal/cli"
 )
 
-func main() {
+func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Println(".env file not found, using system environment variables")
+		log.Println("No .env file found, using system environment variables")
 	}
 
-	cmd.Execute()
+	mode := intres.ResolveMode()
+	if intapp.ModeService == mode || intapp.ModeUtility == mode {
+		_ = godotenv.Load(".env." + mode)
+	}
+}
+
+func main() {
+	ctx, cancel := intres.ResolveContext()
+	defer cancel()
+
+	app, err := intapp.New(ctx, cancel)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to create application: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = intcli.Execute(app)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to execute command: %v\n", err)
+		os.Exit(1)
+	}
 }
