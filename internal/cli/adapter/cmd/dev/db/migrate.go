d892b05f2b48bf7a932b37ba91eb722832631caf
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-17 21:13:03 +0400

    Create migrate and rollback adapters in cli package

---
+++ internal/cli/adapter/cmd/dev/db/migrate.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"github.com/spf13/cobra"
+
+	intapp "github.com/kylerqws/chatbot/internal/app"
+	inthlp "github.com/kylerqws/chatbot/internal/cli/helper"
+	intmig "github.com/kylerqws/chatbot/internal/db/migrator"
+
+	ctr "github.com/kylerqws/chatbot/internal/cli/contract"
+)
+
+type MigrateAdapter struct {
+	*inthlp.CommandAdapterHelper
+}
+
+func NewMigrateAdapter(app *intapp.App) ctr.CommandAdapter {
+	adp := &MigrateAdapter{}
+	cmd := &cobra.Command{}
+
+	adp.CommandAdapterHelper =
+		inthlp.NewCommandAdapterHelper(adp, app, cmd)
+
+	return adp
+}
+
+func (a *MigrateAdapter) Configure() *cobra.Command {
+	a.SetUse("migrate")
+	a.SetShort("Run database schema migrations for the application")
+	a.SetFuncRunE(a.FuncRunE)
+
+	return a.MainConfigure()
+}
+
+func (a *MigrateAdapter) FuncRunE(_ *cobra.Command, _ []string) error {
+	app := a.App()
+
+	err := intmig.Migrate(app.Context(), app.DB())
+	if err != nil {
+		a.AddError(err)
+	}
+
+	if !a.ExistErrors() {
+		return a.PrintMessage("Database migrations have been applied successfully.")
+	}
+	if !a.ShowErrors() {
+		return a.PrintMessage("Database migrations have not been applied.")
+	}
+
+	return a.PrintErrors()
+}
---
+++ internal/cli/adapter/cmd/dev/db/rollback.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"github.com/spf13/cobra"
+
+	intapp "github.com/kylerqws/chatbot/internal/app"
+	inthlp "github.com/kylerqws/chatbot/internal/cli/helper"
+	intmig "github.com/kylerqws/chatbot/internal/db/migrator"
+
+	ctr "github.com/kylerqws/chatbot/internal/cli/contract"
+)
+
+type RollbackAdapter struct {
+	*inthlp.CommandAdapterHelper
+}
+
+func NewRollbackAdapter(app *intapp.App) ctr.CommandAdapter {
+	adp := &RollbackAdapter{}
+	cmd := &cobra.Command{}
+
+	adp.CommandAdapterHelper =
+		inthlp.NewCommandAdapterHelper(adp, app, cmd)
+
+	return adp
+}
+
+func (a *RollbackAdapter) Configure() *cobra.Command {
+	a.SetUse("rollback")
+	a.SetShort("Revert the last set of applied database migrations")
+	a.SetFuncRunE(a.FuncRunE)
+
+	return a.MainConfigure()
+}
+
+func (a *RollbackAdapter) FuncRunE(_ *cobra.Command, _ []string) error {
+	app := a.App()
+
+	err := intmig.Migrate(app.Context(), app.DB())
+	if err != nil {
+		a.AddError(err)
+	}
+
+	if !a.ExistErrors() {
+		return a.PrintMessage("Database rollback completed successfully.")
+	}
+	if !a.ShowErrors() {
+		return a.PrintMessage("Database rollback has not been completed.")
+	}
+
+	return a.PrintErrors()
+}
