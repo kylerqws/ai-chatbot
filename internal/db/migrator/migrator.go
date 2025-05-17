fe981d404304702d9479f42161d9406a78fd51f0
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-17 13:15:33 +0400

    Refactor internal db package

---
+++ internal/db/migrator/migrator.go
@@ -0,0 +1,62 @@
+package migrator
+
+import (
+	"context"
+	"github.com/uptrace/bun/migrate"
+
+	ctr "github.com/kylerqws/chatbot/pkg/db/contract"
+	ctrcl "github.com/kylerqws/chatbot/pkg/db/contract/client"
+)
+
+func Migrate(ctx context.Context, db ctr.DB) (err error) {
+	cl := db.Client()
+	if err = cl.Connect(); err != nil {
+		return err
+	}
+
+	defer func(cl ctrcl.Client) {
+		if clerr := cl.Close(); clerr != nil {
+			if err == nil {
+				err = clerr
+			}
+		}
+	}(cl)
+
+	err = db.Migrator().Migrate(ctx, migrations())
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func Rollback(ctx context.Context, db ctr.DB) (err error) {
+	cl := db.Client()
+	if err = cl.Connect(); err != nil {
+		return err
+	}
+
+	defer func(cl ctrcl.Client) {
+		if clerr := cl.Close(); clerr != nil {
+			if err == nil {
+				err = clerr
+			}
+		}
+	}(cl)
+
+	err = db.Migrator().Rollback(ctx, migrations())
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func migrations() *migrate.Migrations {
+	migs := migrate.NewMigrations()
+	for _, fn := range migrationRegistry {
+		fn(migs)
+	}
+
+	return migs
+}
---
+++ internal/db/migrator/registry.go
@@ -0,0 +1,14 @@
+package migrator
+
+import (
+	intmig "github.com/kylerqws/chatbot/internal/db/migrator/migrations"
+	bunmig "github.com/uptrace/bun/migrate"
+)
+
+var migrationRegistry = []func(*bunmig.Migrations){
+	// --- 06.03.2025 ---
+	intmig.CreateMessageTable06032025001,
+	intmig.CreateServiceTable06032025002,
+	intmig.CreateUserTable06032025003,
+	intmig.CreateAiChatTable06032025004,
+}
