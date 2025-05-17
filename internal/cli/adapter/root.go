4d4a91b5cab04dce10dd80472ed326f474f5e975
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-17 20:10:39 +0400

    Refactor root adapter

--- internal/cli/adapter/root.go
+++ internal/cli/adapter/root.go
@@ -4,46 +4,34 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 
-	"github.com/kylerqws/chatbot/internal/app"
-	"github.com/kylerqws/chatbot/internal/cli/setup"
+	intcmd "github.com/kylerqws/chatbot/cmd"
+	intapp "github.com/kylerqws/chatbot/internal/app"
+	inthlp "github.com/kylerqws/chatbot/internal/cli/helper"
 
-	"github.com/kylerqws/chatbot/cmd"
 	ctr "github.com/kylerqws/chatbot/internal/cli/contract"
 )
 
 type RootAdapter struct {
-	app      *app.App
-	command  *cobra.Command
-	children []*cobra.Command
+	*inthlp.RootAdapterHelper
 }
 
-func NewRootAdapter(app *app.App) ctr.RootAdapter {
-	return &RootAdapter{app: app}
+func NewRootAdapter(app *intapp.App) ctr.RootAdapter {
+	adp := &RootAdapter{}
+	cmd := &cobra.Command{}
+
+	adp.RootAdapterHelper =
+		inthlp.NewRootAdapterHelper(adp, app, cmd)
+
+	return adp
 }
 
 func (a *RootAdapter) Configure() *cobra.Command {
-	a.command = &cobra.Command{
-		Use:     "chatbot",
-		Short:   fmt.Sprintf("CLI for managing %s", a.app.Name()),
-		Version: fmt.Sprintf("v%s", a.app.Version()),
-	}
+	app := a.App()
 
-	a.children = []*cobra.Command{
-		cmd.OpenAICommand(a.app),
-		cmd.DevCommand(a.app),
-	}
+	a.SetUse("chatbot")
+	a.SetShort(fmt.Sprintf("CLI for managing %s", app.Name()))
+	a.SetVersion(fmt.Sprintf("v%s", app.Version()))
+	a.AddChildren(intcmd.OpenAICommand(app), intcmd.DevCommand(app))
 
-	return setup.RootConfigure(a)
-}
-
-func (a *RootAdapter) App() *app.App {
-	return a.app
-}
-
-func (a *RootAdapter) Command() *cobra.Command {
-	return a.command
-}
-
-func (a *RootAdapter) Children() []*cobra.Command {
-	return a.children
+	return a.MainConfigure()
 }
