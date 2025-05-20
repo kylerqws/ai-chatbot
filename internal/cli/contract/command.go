94c3950aee44d3d1c56fde34bc189df395ca9059
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-20 18:38:00 +0400

    Create command contract

---
+++ internal/cli/contract/command.go
@@ -0,0 +1,9 @@
+package contract
+
+import "github.com/spf13/cobra"
+
+type (
+	FuncArgs func(*cobra.Command, []string) error
+	FuncRunE func(*cobra.Command, []string) error
+	FuncHelp func(*cobra.Command, []string)
+)
