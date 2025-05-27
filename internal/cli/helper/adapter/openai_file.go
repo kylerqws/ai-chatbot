b6cfa84500e102bb1a5dbd12b40064884c15aaa4
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-27 21:33:35 +0400

    Create openai-file command adapter helper

---
+++ internal/cli/helper/adapter/openai_file.go
@@ -0,0 +1,71 @@
+package adapter
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/kylerqws/chatbot/pkg/openai/contract/service"
+	"github.com/spf13/cobra"
+)
+
+type File struct {
+	*service.File
+	ExecStatus bool
+}
+
+type OpenAiFileAdapter struct {
+	command *cobra.Command
+	files   []*File
+}
+
+func NewOpenAiFileAdapter(cmd *cobra.Command) *OpenAiFileAdapter {
+	return &OpenAiFileAdapter{command: cmd}
+}
+
+func (h *OpenAiFileAdapter) Files() []*File {
+	return h.files
+}
+
+func (h *OpenAiFileAdapter) ExistFiles() bool {
+	return len(h.files) > 0
+}
+
+func (*OpenAiFileAdapter) WrapOpenAIFile(file *service.File) *File {
+	return &File{File: file}
+}
+
+func (h *OpenAiFileAdapter) WrapOpenAIFiles(files ...*service.File) []*File {
+	wraps := make([]*File, len(files))
+	for i := range files {
+		wraps = append(wraps, h.WrapOpenAIFile(files[i]))
+	}
+	return wraps
+}
+
+func (h *OpenAiFileAdapter) AddFile(file *File) {
+	if file != nil {
+		h.files = append(h.files, file)
+	}
+}
+
+func (h *OpenAiFileAdapter) AddFiles(files ...*File) {
+	for i := range files {
+		h.AddFile(files[i])
+	}
+}
+
+func (*OpenAiFileAdapter) FileSize(path string) int64 {
+	info, err := os.Stat(path)
+	if err != nil || info.IsDir() {
+		return 0
+	}
+	return info.Size()
+}
+
+func (*OpenAiFileAdapter) ValidateFileID(fileID string) error {
+	if !regexp.MustCompile(`^file-[a-zA-Z0-9]{22}$`).MatchString(fileID) {
+		return fmt.Errorf("invalid file ID %q", fileID)
+	}
+	return nil
+}
