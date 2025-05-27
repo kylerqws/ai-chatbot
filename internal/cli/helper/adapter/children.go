a47b63861d6ee9b7e79a6e24bfdfbc5abc445e1d
Rodion Kachkin <kyleRQWS@gmail.com>
2025-05-27 10:35:11 +0400

    Rename name children adapter helper

--- internal/cli/helper/adapter/children.go
+++ internal/cli/helper/adapter/children.go
@@ -2,33 +2,31 @@ package adapter
 
 import "github.com/spf13/cobra"
 
-type ChildrenAdapterHelper struct {
+type ChildrenAdapter struct {
 	command  *cobra.Command
 	children []*cobra.Command
 }
 
-func NewChildrenAdapterHelper(cmd *cobra.Command) *ChildrenAdapterHelper {
-	return &ChildrenAdapterHelper{command: cmd}
+func NewChildrenAdapter(cmd *cobra.Command) *ChildrenAdapter {
+	return &ChildrenAdapter{command: cmd}
 }
 
-func (h *ChildrenAdapterHelper) Children() []*cobra.Command {
+func (h *ChildrenAdapter) Children() []*cobra.Command {
 	return h.children
 }
 
-func (h *ChildrenAdapterHelper) ExistChildren() bool {
+func (h *ChildrenAdapter) ExistChildren() bool {
 	return len(h.children) > 0
 }
 
-func (h *ChildrenAdapterHelper) AddChild(cmd *cobra.Command) {
+func (h *ChildrenAdapter) AddChild(cmd *cobra.Command) {
 	if cmd != nil {
 		h.children = append(h.children, cmd)
 	}
 }
 
-func (h *ChildrenAdapterHelper) AddChildren(cmds ...*cobra.Command) {
+func (h *ChildrenAdapter) AddChildren(cmds ...*cobra.Command) {
 	for i := range cmds {
-		if cmds[i] != nil {
-			h.children = append(h.children, cmds[i])
-		}
+		h.AddChild(cmds[i])
 	}
 }
