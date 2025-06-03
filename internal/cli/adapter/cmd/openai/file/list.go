acc03148ac09c4fcd6e15a1e052e006ef28692db
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-03 14:42:44 +0400

    Minor fixes file.list adapter

--- internal/cli/adapter/cmd/openai/file/list.go
+++ internal/cli/adapter/cmd/openai/file/list.go
@@ -13,27 +13,12 @@ import (
 	ctrsvc "github.com/kylerqws/chatbot/pkg/openai/contract/service"
 )
 
-const (
-	idFlagKey            = "id"
-	statusFlagKey        = "status"
-	purposeFlagKey       = "purpose"
-	filenameFlagKey      = "filename"
-	createdAfterFlagKey  = "created-after"
-	createdBeforeFlagKey = "created-before"
-)
-
-const (
-	fileIDExample    = "file-xxxxxx..."
-	filename1Example = "step_metrics.csv"
-	filename2Example = "prompts.json"
-	dateExample      = "1970-01-01"
-	datetimeExample  = "1970-01-01 00:00:00"
-)
-
 type ListAdapter struct {
 	*helper.CommandAdapter
 	*helper.OpenAiAdapter
 	*helper.OpenAiFileAdapter
+	*helper.PaginationAdapter
+	*helper.FilterAdapter
 	*helper.FlagAdapter
 	*helper.ValidateAdapter
 	*helper.DateTimeAdapter
@@ -48,6 +33,8 @@ func NewListAdapter(app *intapp.App) ctradp.CommandAdapter {
 	adp.CommandAdapter = helper.NewCommandAdapter(app, cmd)
 	adp.OpenAiAdapter = helper.NewOpenAiAdapter(cmd)
 	adp.OpenAiFileAdapter = helper.NewOpenAiFileAdapter(cmd)
+	adp.PaginationAdapter = helper.NewPaginationAdapter(cmd)
+	adp.FilterAdapter = helper.NewFilterAdapter(cmd)
 	adp.FlagAdapter = helper.NewFlagAdapter(cmd)
 	adp.ValidateAdapter = helper.NewValidateAdapter(cmd)
 	adp.DateTimeAdapter = helper.NewDateTimeAdapter(cmd)
@@ -60,44 +47,68 @@ func NewListAdapter(app *intapp.App) ctradp.CommandAdapter {
 func (a *ListAdapter) Configure() *cobra.Command {
 	a.SetUse("list [filter-flag...]")
 	a.SetShort("Display files in OpenAI account")
-	a.SetLong(a.info())
+	a.SetLong(a.HelpInfo())
 
 	a.SetFuncArgs(a.Validate)
 	a.SetFuncRunE(a.List)
 
+	a.ConfigureFilters()
 	a.ConfigureFlags()
+
 	return a.MainConfigure()
 }
 
+func (a *ListAdapter) ConfigureFilters() {
+	a.AddFilterKeys(
+		helper.IdFlagKey,
+		helper.StatusFlagKey,
+		helper.PurposeFlagKey,
+		helper.FilenameFlagKey,
+		helper.CreatedAfterFlagKey,
+		helper.CreatedBeforeFlagKey,
+		helper.AfterFlagKey,
+		helper.LimitFlagKey,
+	)
+}
+
 func (a *ListAdapter) ConfigureFlags() {
-	desc := "Filter by file ID (e.g. " + fileIDExample + ")"
-	a.AddStringSliceFlag(idFlagKey, "", []string{}, desc)
+	desc := "Filter by file ID (e.g. " + helper.FileIDExample + ")"
+	a.AddStringSliceFlag(helper.IdFlagKey, "", []string{}, desc)
 
 	desc = "Filter by status (e.g. " + a.FileStatusManager().JoinCodes(", ") + ")"
-	a.AddStringSliceFlag(statusFlagKey, "", []string{}, desc)
+	a.AddStringSliceFlag(helper.StatusFlagKey, "", []string{}, desc)
 
 	desc = "Filter by purpose (e.g. " + a.PurposeManager().JoinCodes(", ") + ")"
-	a.AddStringSliceFlag(purposeFlagKey, "", []string{}, desc)
+	a.AddStringSliceFlag(helper.PurposeFlagKey, "", []string{}, desc)
 
-	desc = "Filter by file name (e.g. " + filename1Example + ", " + filename2Example + ")"
-	a.AddStringSliceFlag(filenameFlagKey, "", []string{}, desc)
+	desc = "Filter by file name (e.g. " + helper.Filename1Example + ", " + helper.Filename2Example + "...)"
+	a.AddStringSliceFlag(helper.FilenameFlagKey, "", []string{}, desc)
 
-	desc = "Filter by creation date after (e.g. " + dateExample + " or " + datetimeExample + ")"
-	a.AddStringFlag(createdAfterFlagKey, "", a.DateTime(0, -3, 0), desc)
+	desc = "Filter by creation date after (e.g. " + helper.DateExample + " or " + helper.DatetimeExample + ")"
+	a.AddStringFlag(helper.CreatedAfterFlagKey, "", "", desc)
 
-	desc = "Filter by creation date before (e.g. " + dateExample + " or " + datetimeExample + ")"
-	a.AddStringFlag(createdBeforeFlagKey, "", "", desc)
+	desc = "Filter by creation date before (e.g. " + helper.DateExample + " or " + helper.DatetimeExample + ")"
+	a.AddStringFlag(helper.CreatedBeforeFlagKey, "", "", desc)
+
+	desc = "After file ID (e.g. " + helper.FileIDExample + ")"
+	a.AddStringFlag(helper.AfterFlagKey, "", "", desc)
+
+	desc = "Limit files (" + a.JoinLimits(", ") + ")"
+	a.AddUint8Flag(helper.LimitFlagKey, "", helper.DefaultLimit, desc)
 }
 
 func (a *ListAdapter) Validate(_ *cobra.Command, _ []string) error {
-	a.AddErrors(a.ValidateStringSliceFlag(idFlagKey, a.ValidateFileID)...)
+	a.AddErrors(a.ValidateStringSliceFlag(helper.IdFlagKey, a.ValidateFileID)...)
 
-	a.AddErrors(a.ValidateStringSliceFlag(statusFlagKey, a.ValidateFileStatusCode)...)
-	a.AddErrors(a.ValidateStringSliceFlag(purposeFlagKey, a.ValidatePurposeCode)...)
+	a.AddErrors(a.ValidateStringSliceFlag(helper.StatusFlagKey, a.ValidateFileStatusCode)...)
+	a.AddErrors(a.ValidateStringSliceFlag(helper.PurposeFlagKey, a.ValidatePurposeCode)...)
 
 	a.AddErrors(
-		a.ValidateStringFlag(createdAfterFlagKey, a.ValidateDateFormat),
-		a.ValidateStringFlag(createdBeforeFlagKey, a.ValidateDateFormat),
+		a.ValidateStringFlag(helper.CreatedAfterFlagKey, a.ValidateDateFormat),
+		a.ValidateStringFlag(helper.CreatedBeforeFlagKey, a.ValidateDateFormat),
+
+		a.ValidateStringFlag(helper.AfterFlagKey, a.ValidateFileID),
+		a.ValidateUint8Flag(helper.LimitFlagKey, a.ValidateLimit),
 	)
 
 	return a.ErrorIfExist("One or more arguments/flags are invalid or missing.")
@@ -121,32 +132,42 @@ func (a *ListAdapter) Request() bool {
 	svc := app.OpenAI().FileService()
 	fgs := a.Command().Flags()
 
-	fileIDs, err := fgs.GetStringSlice(idFlagKey)
+	fileIDs, err := fgs.GetStringSlice(helper.IdFlagKey)
 	if err != nil {
 		a.AddError(err)
 	}
 
-	statuses, err := fgs.GetStringSlice(statusFlagKey)
+	statuses, err := fgs.GetStringSlice(helper.StatusFlagKey)
 	if err != nil {
 		a.AddError(err)
 	}
 
-	purposes, err := fgs.GetStringSlice(purposeFlagKey)
+	purposes, err := fgs.GetStringSlice(helper.PurposeFlagKey)
 	if err != nil {
 		a.AddError(err)
 	}
 
-	filenames, err := fgs.GetStringSlice(filenameFlagKey)
+	filenames, err := fgs.GetStringSlice(helper.FilenameFlagKey)
 	if err != nil {
 		a.AddError(err)
 	}
 
-	afterStr, err := fgs.GetString(createdAfterFlagKey)
+	afterStr, err := fgs.GetString(helper.CreatedAfterFlagKey)
 	if err != nil {
 		a.AddError(err)
 	}
 
-	beforeStr, err := fgs.GetString(createdBeforeFlagKey)
+	beforeStr, err := fgs.GetString(helper.CreatedBeforeFlagKey)
+	if err != nil {
+		a.AddError(err)
+	}
+
+	afterFileID, err := fgs.GetString(helper.AfterFlagKey)
+	if err != nil {
+		a.AddError(err)
+	}
+
+	limitFiles, err := fgs.GetUint8(helper.LimitFlagKey)
 	if err != nil {
 		a.AddError(err)
 	}
@@ -158,6 +179,8 @@ func (a *ListAdapter) Request() bool {
 		Filenames:     filenames,
 		CreatedAfter:  a.ParseDateTime(afterStr),
 		CreatedBefore: a.ParseDateTime(beforeStr),
+		AfterFileID:   afterFileID,
+		LimitFiles:    limitFiles,
 	})
 
 	if err != nil {
@@ -172,15 +195,15 @@ func (a *ListAdapter) PrintFiles() error {
 	_ = a.CreateTable()
 
 	a.AppendTableHeader("File ID", "File Name", "Purpose",
-		"Size", "Status", "Created")
+		"Size", "Created", "Status")
 
 	a.SetColumnTableConfigs(
 		a.ColumnConfig(1, text.AlignCenter, 27, text.Colors{text.Bold}),
 		a.ColumnConfig(2, text.AlignRight, 19, nil),
 		a.ColumnConfig(3, text.AlignRight, 19, nil),
 		a.ColumnConfig(4, text.AlignRight, 10, nil),
-		a.ColumnConfig(5, text.AlignRight, 10, nil),
-		a.ColumnConfig(6, text.AlignRight, 19, nil),
+		a.ColumnConfig(5, text.AlignRight, 19, nil),
+		a.ColumnConfig(6, text.AlignRight, 10, text.Colors{text.Bold}),
 	)
 
 	files := a.Files()
@@ -192,8 +215,8 @@ func (a *ListAdapter) PrintFiles() error {
 			a.FormatString(files[i].Filename, &empty),
 			a.FormatString(files[i].Purpose, &empty),
 			a.FormatBytes(files[i].Bytes, &empty),
-			a.FormatString(files[i].Status, &empty),
 			a.FormatTime(files[i].CreatedAt, &empty),
+			a.FormatString(files[i].Status, &empty),
 		)
 	}
 
@@ -201,7 +224,7 @@ func (a *ListAdapter) PrintFiles() error {
 	return nil
 }
 
-func (a *ListAdapter) info() string {
+func (a *ListAdapter) HelpInfo() string {
 	cmdName := a.Command().Name()
 	prpManager := a.PurposeManager()
 
@@ -209,8 +232,8 @@ func (a *ListAdapter) info() string {
 		fmt.Sprintf(
 			"  %s --%s %s --%s %s --%s %s",
 			cmdName,
-			purposeFlagKey, prpManager.Codes.FineTune,
-			purposeFlagKey, prpManager.Codes.Evals,
-			createdAfterFlagKey, a.Date(0, 0, -7),
+			helper.PurposeFlagKey, prpManager.Codes.FineTune,
+			helper.PurposeFlagKey, prpManager.Codes.Evals,
+			helper.CreatedAfterFlagKey, a.Date(0, 0, -7),
 		)
 }
