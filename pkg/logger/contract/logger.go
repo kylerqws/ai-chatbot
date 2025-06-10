2ddcfc98b82ab24144bda9e5fa29cce78e2cd5da
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-10 15:07:37 +0400

    Fix code style in main logger contract

--- pkg/logger/contract/logger.go
+++ pkg/logger/contract/logger.go
@@ -6,16 +6,19 @@ import "context"
 type Logger interface {
 	// Info logs an informational message.
 	Info(args ...any)
+
 	// InfoWithContext logs an informational message with context.
 	InfoWithContext(ctx context.Context, args ...any)
 
 	// Error logs an error message.
 	Error(args ...any)
+
 	// ErrorWithContext logs an error message with context.
 	ErrorWithContext(ctx context.Context, args ...any)
 
 	// Debug logs a debug-level message.
 	Debug(args ...any)
+
 	// DebugWithContext logs a debug-level message with context.
 	DebugWithContext(ctx context.Context, args ...any)
 }
