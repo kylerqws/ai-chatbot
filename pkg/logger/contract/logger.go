e343d4ea6310d294229b7556d197652c4820492a
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-10 14:37:00 +0400

    Align Logger interface doc style

--- pkg/logger/contract/logger.go
+++ pkg/logger/contract/logger.go
@@ -2,13 +2,20 @@ package contract
 
 import "context"
 
+// Logger provides structured logging with context support.
 type Logger interface {
-	Info(...any)
+	// Info logs an informational message.
+	Info(args ...any)
+	// InfoWithContext logs an informational message with context.
 	InfoWithContext(ctx context.Context, args ...any)
 
-	Error(...any)
+	// Error logs an error message.
+	Error(args ...any)
+	// ErrorWithContext logs an error message with context.
 	ErrorWithContext(ctx context.Context, args ...any)
 
-	Debug(...any)
+	// Debug logs a debug-level message.
+	Debug(args ...any)
+	// DebugWithContext logs a debug-level message with context.
 	DebugWithContext(ctx context.Context, args ...any)
 }
