c5b3d2be67a63def9cb728e6019cde77b8fa621a
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-10 22:29:42 +0400

    Add docs to dev.db commands

--- cmd/dev/db/migrate.go
+++ cmd/dev/db/migrate.go
@@ -7,6 +7,7 @@ import (
 	intcli "github.com/kylerqws/chatbot/internal/cli/adapter/cmd/dev/db"
 )
 
+// MigrateCommand creates a command for applies database migrations.
 func MigrateCommand(app *intapp.App) *cobra.Command {
 	return intcli.NewMigrateAdapter(app).Configure()
 }
--- cmd/dev/db/rollback.go
+++ cmd/dev/db/rollback.go
@@ -7,6 +7,7 @@ import (
 	intcli "github.com/kylerqws/chatbot/internal/cli/adapter/cmd/dev/db"
 )
 
+// RollbackCommand creates a command for rollback database migrations.
 func RollbackCommand(app *intapp.App) *cobra.Command {
 	return intcli.NewRollbackAdapter(app).Configure()
 }
