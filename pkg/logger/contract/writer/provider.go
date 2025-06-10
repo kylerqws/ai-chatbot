1bec956028fcfeab686b2e6e2ef90e1fae71d62d
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-10 14:47:45 +0400

    Align writer provider interface with project conventions

--- pkg/logger/contract/writer/provider.go
+++ pkg/logger/contract/writer/provider.go
@@ -2,6 +2,15 @@ package writer
 
 import "io"
 
+// Writer types used to identify logging destinations.
+const (
+	TypeStdout = "stdout"
+	TypeStderr = "stderr"
+	TypeDB     = "db"
+)
+
+// Provider provides an io.Writer for logger output.
 type Provider interface {
+	// Writer returns the configured io.Writer.
 	Writer() io.Writer
 }
