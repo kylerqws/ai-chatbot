61cf6e975750288c0dae7c2bae170b23179e198d
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-10 14:41:15 +0400

    Align zerolog implementation with contract style and project conventions

--- pkg/logger/infrastructure/logger/zerolog.go
+++ pkg/logger/infrastructure/logger/zerolog.go
@@ -11,63 +11,70 @@ import (
 	ctrcfg "github.com/kylerqws/chatbot/pkg/logger/contract/config"
 )
 
+// zeroLogger is a zerolog-based implementation of the Logger interface.
 type zeroLogger struct {
 	config ctrcfg.Config
 	logger *zerolog.Logger
 }
 
+// NewZeroLogger creates and returns a new zerolog-based logger instance.
 func NewZeroLogger(cfg ctrcfg.Config, w io.Writer) ctr.Logger {
 	zl := zerolog.New(w).With().Timestamp().Logger()
 	return &zeroLogger{config: cfg, logger: &zl}
 }
 
+// Info logs an informational message.
 func (l *zeroLogger) Info(args ...any) {
 	l.logger.Info().Msg(l.format(args...))
 }
 
+// InfoWithContext logs an informational message with context.
 func (l *zeroLogger) InfoWithContext(ctx context.Context, args ...any) {
-	l.from(ctx).Info().Msg(l.format(args...))
+	l.withContext(ctx).Info().Msg(l.format(args...))
 }
 
+// Error logs an error message.
 func (l *zeroLogger) Error(args ...any) {
 	l.logger.Error().Msg(l.format(args...))
 }
 
+// ErrorWithContext logs an error message with context.
 func (l *zeroLogger) ErrorWithContext(ctx context.Context, args ...any) {
-	l.from(ctx).Error().Msg(l.format(args...))
+	l.withContext(ctx).Error().Msg(l.format(args...))
 }
 
+// Debug logs a debug-level message if debug mode is enabled.
 func (l *zeroLogger) Debug(args ...any) {
 	if l.config.IsDebug() {
 		l.logger.Debug().Msg(l.format(args...))
 	}
 }
 
+// DebugWithContext logs a debug-level message with context if debug mode is enabled.
 func (l *zeroLogger) DebugWithContext(ctx context.Context, args ...any) {
 	if l.config.IsDebug() {
-		l.from(ctx).Debug().Msg(l.format(args...))
+		l.withContext(ctx).Debug().Msg(l.format(args...))
 	}
 }
 
-func (l *zeroLogger) from(ctx context.Context) *zerolog.Logger {
+// withContext returns a logger enriched with context, if available.
+func (l *zeroLogger) withContext(ctx context.Context) *zerolog.Logger {
 	if ctx == nil {
 		return l.logger
 	}
-
 	if ctxLogger := zerolog.Ctx(ctx); ctxLogger != nil {
 		logger := ctxLogger.With().Logger()
 		return &logger
 	}
-
 	return l.logger
 }
 
+// format joins arguments into a single message string.
 func (*zeroLogger) format(args ...any) string {
 	if len(args) == 1 {
 		if str, ok := args[0].(string); ok {
 			return str
 		}
 	}
-
 	return fmt.Sprint(args...)
 }
