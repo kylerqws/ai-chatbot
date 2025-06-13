3ee6ca471216be6acc1d5b6846e1db887c5b34e7
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-14 00:45:27 +0400

    Add doc to contract in internal/db package

--- internal/db/contract/db.go
+++ internal/db/contract/db.go
@@ -7,6 +7,9 @@ import (
 
 // DB defines the internal abstraction over the database client.
 type DB interface {
+	// Client returns the database client instance.
 	Client() ctrcl.Client
+
+	// Migrator returns the database migration manager.
 	Migrator() ctrmig.Migrator
 }
