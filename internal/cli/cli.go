bd1cc43e6c2fa36f64b8a50548687183545a6c56
Rodion Kachkin <kyleRQWS@gmail.com>
2025-06-14 12:47:22 +0400

    Add docs to internal/cli entrypoint

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -7,11 +7,16 @@ import (
 	"github.com/kylerqws/chatbot/internal/cli/adapter"
 )
 
+// RootCommand creates the root CLI command.
 func RootCommand(app *app.App) *cobra.Command {
+	// Disable automatic sorting of subcommands to preserve custom order.
 	cobra.EnableCommandSorting = false
+
 	return adapter.NewRootAdapter(app).Configure()
 }
 
+// Execute runs the root CLI command.
 func Execute(app *app.App) error {
+	defer app.ContextCancel()()
 	return RootCommand(app).Execute()
 }
